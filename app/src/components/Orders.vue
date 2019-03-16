c9249da4cb24cbb08826dea5f97769ef158a0aff
Daniel Milner <danielmmilner@gmail.com>
2019-03-17 03:32:37 +1100

    fixed errors

--- app/src/components/Orders.vue
+++ app/src/components/Orders.vue
@@ -1,32 +1,23 @@
 <template>
   <div>
     <div v-for="(booking, key) in restaurant.bookings" :key="booking.id" class="column">
-        <Order :restaurantId="restaurant.id" :bookingId="key" v-bind:booking="booking" />
+      <Order :restaurantId="restaurant.id" :bookingId="key" v-bind:booking="booking"/>
     </div>
   </div>
 </template>
 
 <script>
-import db from '@/firestore'
-
-import Order from '@/components/Order'
+import Order from "@/components/Order";
 
 export default {
-    props: ['restaurant'],
-    name: 'orders',
+  props: ["restaurant"],
+  name: "orders",
 
-    components: {
-        Order
-    },
-
-    methods: {
-      ready (index, newBooking) {
-        db.collection('restaurants').doc('')
-      }
-    }
-}
+  components: {
+    Order
+  }
+};
 </script>
 
 <style>
-
 </style>
